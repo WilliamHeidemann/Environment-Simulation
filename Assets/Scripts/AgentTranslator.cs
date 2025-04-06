50d25b5d0ce33b780b7fa52c5760fb91f77d9c0d
William Skou Heidemann <williamskou2227@gmail.com>
2025-04-06 21:00:24 +0200

    remove commented out code

--- Assets/Scripts/AgentTranslator.cs
+++ Assets/Scripts/AgentTranslator.cs
@@ -27,37 +27,7 @@ public class AgentTranslator : MonoBehaviour
 
     private void Update()
     {
-        // for (int i = 0; i < _agentsData.Transforms.Length; i++)
-        // {
-        //     AgentTransform agentTransform = _agentsData.Transforms[i];
-        //     AgentMotion agentMotion = _agentsData.Motions[i];
-        //     var velocity = agentMotion.Velocity.normalized * (agentMotion.Speed * Time.deltaTime);
-        //     float height = _terrain.SampleHeight(agentTransform.Position + velocity);
-        //     agentMotion.Velocity = new Vector3(agentMotion.Velocity.x, height - agentTransform.Position.y, agentMotion.Velocity.z); 
-        //     _agentsData.Motions[i] = agentMotion;
-        // }
-
         _translateJob.DeltaTime = Time.deltaTime;
         _translateJob.Schedule(_count, 1).Complete();
-
-        // var terrainData = _terrain.terrainData;
-        //
-        // for (int i = 0; i < _agentsData.Transforms.Length; i++)
-        // {
-        //     AgentTransform agentTransform = _agentsData.Transforms[i];
-        //     float height = _terrain.SampleHeight(agentTransform.Position);
-        //     agentTransform.Position = new Vector3(agentTransform.Position.x, height, agentTransform.Position.z);
-        //
-        //     AgentMotion agentMotion = _agentsData.Motions[i];
-        //     var velocity = agentMotion.Velocity;
-        //     var terrainPosition = agentTransform.Position - _terrain.transform.position;
-        //     var u = terrainPosition.x / terrainData.size.x;
-        //     var v = terrainPosition.z / terrainData.size.z;
-        //     var terrainNormal = terrainData.GetInterpolatedNormal(u, v);
-        //     var projection = Vector3.ProjectOnPlane(velocity, terrainNormal);
-        //     var rotation = Quaternion.LookRotation(projection, terrainNormal);
-        //     agentTransform.Rotation = rotation;
-        //     _agentsData.Transforms[i] = agentTransform;
-        // }
     }
 }
